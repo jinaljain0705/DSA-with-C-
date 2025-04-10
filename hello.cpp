387d7a97bd95fadd6285c6c7cb8c28a8af890a9f
Jinal Jain <jinaljain624@gmail.com>
2025-04-10 06:20:39 +0530

    commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,l,sum;
+    cout<<"enter the value of n and l";
+    cin>>n>>l;
+    sum = n + l;
+    cout<<"sum of n and l is: "<<sum ;
+    return 0;
+}
